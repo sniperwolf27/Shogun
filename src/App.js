94752a7ccf1a83ae6140903082a39b299b481dae
sniperwolf27 <dayanpr2728@gmail.com>
2024-02-20 16:08:56 -0400

    Add react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/styles/Index.css';
-
+import { Link, BrowserRouter, Route,Routes  } from 'react-router-dom';
+import Catalog from './components/Catalog';
+import Product from './components/Product';
+import NavBar from './components/NavBar';
+import ProductData from './data/products.json';
 function App() {
   return (
-    <div className="App">
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Catalog products={ProductData}/>} />
+        <Route path="/product/:id" element={<Catalog products={ProductData}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
